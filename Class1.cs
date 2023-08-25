a4d8337d358d9372fb0853884b1a08e983495e69
malik cisse <cissemalik@hotmail.com>
2023-08-25 15:01:59 +0200

    Ajoutez des fichiers projet.

---
+++ Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace TDD_Exercice4
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Exo4_RechercheVille.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace TDD_Exercice4
+{
+    public class Exo4_RechercheVille
+    {
+        private List<String> _villes = new List<string>() { "Paris", "Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York", "Londres", "Bangkok", "Hong Kong", "Dubaï", "Rome", "Istanbul" };
+
+        public List<String> Rechercher(String mot)
+        {
+            if (mot.Count() < 2)
+                throw new NotFoundException();
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
+// { "Paris", "Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York", "Londres", "Bangkok", "Hong Kong", "Dubaï", "Rome", "Istanbul"}
